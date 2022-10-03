b8acadf6a26aae4c541f9c338a48f86946202f95
jayishembryo <kembry@mail.bradley.edu>
2022-10-03 13:56:47 -0500

    initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+
+    public Rigidbody2D rb2d;
+    public float jumpforce = 10;
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+       
+        //if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+
+
+        }
+
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+
+            rb2d.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+
+        }
+
+        //if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        {
+
+
+
+        }
+
+       // if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+
+
+        }
+
+    }
+}
